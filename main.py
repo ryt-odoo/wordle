4a7ee9efa96e20b659914231dccfe7345fb07467
Ridha <riyounes@gmail.com>
2023-08-29 13:15:04 +0100

    creat gitignor

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import tools
 def main():
-    pass
+    """this game consists of guessing a chosen word from a predefined list
+        the player has only six chances to findout the word."""
+    word = choose_a_word()
+    print (word)
